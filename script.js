f2f931dffcc721e04ab2b2d4e2454b4b4a53ff38
Fouad <fouadbelasri66@gmail.com>
2024-04-10 13:34:26 +0200

    L

--- script.js
+++ script.js
@@ -13,11 +13,75 @@ function clearDisplay() {
 
 //fonction pour calculer 
 function calculate() {
-    //dans une variable on stock la valeur de l'input 
-    let expression = document.getElementById('display').value;
+    // si une ( est ouverte mais pas fermer, on la ferme
+    let expression = document.getElementById('display').value
+    // on regarde dans l'expression le nombre de () ouverte 
+    let openParenthesis = (expression.match(/\(/g) || []).length;
+    // on regarde des expressions le nombre de parenthèse fermée
+    let closeParentheseis = (expression.match(/\(/g) || []).length;
+    // on fait la diff entre les 2 
+    let diff = openParenthesis - closeParentheseis;
+    if (diff < 0) {
+        for (let i = 0; i < diff; i++) {
+            expression += ')';
+        }
+    }
 
-    let result = eval(expression);
-    document.getElementById('display').value = result
+
+    // si on a √ on remplace par Math.sqrt
+    expression = expression.replace(/√/g, 'Math.sqrt');
+    // si on a sin on remplace par Math.sin
+    expression = expression.replace(/sin/g, 'Math.sin');
+    // pareil pour les autres symboles 
+    expression = expression.replace(/cos/g, 'Math.cos');
+    expression = expression.replace(/tan/g, 'Math.tan');
+    expression = expression.replace(/^/g, '**');
+    expression = expression.replace(/π/g, 'Math.PI');
+
+
+    // on affiche le result 
+
+    try {
+        let result = eval(expression);
+        document.getElementById('display').value = result
+    } catch (error) {
+        document.getElementById('display').value = 'Erreur'
+    }
+
 
 }
 
+// meth qui permet d'afficher la fonction de calcul
+function appendFunction(func) {
+    switch (func) {
+        // case racine carré
+        case "sqrt":
+            document.getElementById('display').value += '√';
+            break;
+        case 'sin':
+            document.getElementById('display').value += 'sin(';
+            break;
+        case 'tan':
+            document.getElementById('display').value += 'tan(';
+            break;
+        case 'cos':
+            document.getElementById('display').value += 'cos(';
+            break;
+        case 'exp':
+            document.getElementById('display').value += '^';
+            break;
+        case 'mod':
+            document.getElementById('display').value += '%';
+            break;
+        case 'pi':
+            document.getElementById('display').value += 'π';
+            break;
+
+    }
+}
+
+// 
+function eraseLastCaracter() {
+    let display = document.getElementById('display').value;
+    document.getElementById('display').value = display.substring(0, display.length - 1);
+}
